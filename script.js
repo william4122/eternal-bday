bfec163a544c0e39f1a099949b0088eec4f6a568
william4122 <119632767+william4122@users.noreply.github.com>
2023-12-19 19:47:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -40,21 +40,3 @@ function openYear(year) {
   // For now, we'll just alert the selected year
   alert(`Opening birthday cards for ${year}`);
 }
-
-// Check Passphrase
-function checkPassphrase() {
-  const passphraseInput = document.getElementById('passphrase');
-  const passphrase = passphraseInput.value;
-
-  // Replace with your logic to validate the passphrase
-  if (passphrase === 'your_password') {
-    document.getElementById('passphrase-modal').style.display = 'none';
-  } else {
-    alert('Incorrect passphrase. Try again.');
-  }
-}
-
-// Display Passphrase Modal on page load
-window.onload = function () {
-  document.getElementById('passphrase-modal').style.display = 'flex';
-};

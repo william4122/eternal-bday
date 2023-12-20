ebdce557217cdd413021ffb46bab7a40d25f7ad2
william4122 <119632767+william4122@users.noreply.github.com>
2023-12-19 20:50:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const modalOverlay = document.getElementById('fullscreen-modal-overlay');
 // Toggle Side Navigation Bar
 function toggleNav() {
-  const sideNav = document.getElementById('side-nav');
+  // Your toggleNav function code
+const sideNav = document.getElementById('side-nav');
   if (sideNav.style.left === '0px') {
     sideNav.style.left = '-200px';
   } else {
@@ -27,13 +26,8 @@ function openFullScreen(imageSrc, caption) {
 // Close Fullscreen Modal
 function closeFullScreen() {
   const modal = document.getElementById('fullscreen-modal');
-  const modalOverlay = document.getElementById('fullscreen-modal-overlay'); // Add this line
   modal.style.display = 'none';
-
-  // Remove the event listener to prevent multiple bindings
-  modalOverlay.removeEventListener('click', closeFullScreen);
 }
-
 
 // Open Birthday Year
 function openYear(year) {
@@ -41,18 +35,3 @@ function openYear(year) {
   // For now, we'll just alert the selected year
   alert(`Opening birthday cards for ${year}`);
 }
-// Check Passphrase
-function checkPassphrase() {
-  const passphraseInput = document.getElementById('passphrase');
-  const passphrase = passphraseInput.value;
-  // Replace with your logic to validate the passphrase
-  if (passphrase === 'your_password') {
-    document.getElementById('passphrase-modal').style.display = 'none';
-  } else {
-    alert('Incorrect passphrase. Try again.');
-  }
-}
-// Display Passphrase Modal on page load
-window.onload = function () {
-  document.getElementById('passphrase-modal').style.display = 'flex';
-

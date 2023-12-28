9beab16d6296fb496ebfc1210a505f3d90d01115
william4122 <119632767+william4122@users.noreply.github.com>
2023-12-27 21:42:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -49,12 +49,30 @@ function openYear(year) {
 document.addEventListener('DOMContentLoaded', function () {
   const scrollableBox = document.getElementById('scrollable-box');
 
-  const imageData = [
-    { src: 'images/Image1.jpg', caption: 'Chris happy birthday! I got you a present!' },
-    { src: 'images/Image2.jpg', caption: 'La rata watching 2' },
-    { src: 'images/Image3.jpg', caption: 'NYC pt 3' },
-    // Add more objects for additional images
-  ];
+const imageData = [
+  { src: 'images/Image1.jpg', caption: 'Chris happy birthday! I got you a present!' },
+  { src: 'images/Image2.jpg', caption: 'La rata watching 2' },
+  { src: 'images/Image3.jpg', caption: 'NYC pt 3' },
+  { src: 'images/Image (10).heic', caption: 'Caption for Image (10)' },
+  { src: 'images/Image (11).heic', caption: 'Caption for Image (11)' },
+  { src: 'images/Image (12).heic', caption: 'Caption for Image (12)' },
+  { src: 'images/Image (2).PNG', caption: 'Caption for Image (2)' },
+  { src: 'images/Image (2).heic', caption: 'Caption for Image (2)' },
+  { src: 'images/Image (2).jpeg', caption: 'Caption for Image (2)' },
+  { src: 'images/Image (2).jpg', caption: 'Caption for Image (2)' },
+  { src: 'images/Image (3).HEIC', caption: 'Caption for Image (3)' },
+  { src: 'images/Image (4).HEIC', caption: 'Caption for Image (4)' },
+  { src: 'images/Image (5).HEIC', caption: 'Caption for Image (5)' },
+  { src: 'images/Image (6).HEIC', caption: 'Caption for Image (6)' },
+  { src: 'images/Image (7).heic', caption: 'Caption for Image (7)' },
+  { src: 'images/Image (8).heic', caption: 'Caption for Image (8)' },
+  { src: 'images/Image (9).heic', caption: 'Caption for Image (9)' },
+  { src: 'images/Image.JPG', caption: 'Caption for Image' },
+  { src: 'images/Image4.jpeg', caption: 'Caption for Image4' },
+  { src: 'images/Image5.heic', caption: 'Caption for Image5' },
+  { src: 'images/Image6.PNG', caption: 'Caption for Image6' },
+  // Add more objects for additional images
+];
 
   imageData.forEach((data, index) => {
     const subBox = document.createElement('div');

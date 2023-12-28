330ffe8f66ee1648a7d4b84a7221fb9ee4340285
william4122 <119632767+william4122@users.noreply.github.com>
2023-12-27 21:04:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
 // Toggle Side Navigation Bar
 function toggleNav() {
-  // Your toggleNav function code
-const sideNav = document.getElementById('side-nav');
+  const sideNav = document.getElementById('side-nav');
   if (sideNav.style.left === '0px') {
     sideNav.style.left = '-200px';
   } else {
     sideNav.style.left = '0px';
   }
 }
+
 // Open Fullscreen Modal
 function openFullScreen(imageSrc, caption) {
   const modal = document.getElementById('fullscreen-modal');
   const image = document.getElementById('fullscreen-image');
   const captionElement = document.getElementById('caption');
-  const modalOverlay = document.getElementById('fullscreen-modal-overlay'); // Add this line
+  const modalOverlay = document.getElementById('fullscreen-modal-overlay');
 
   image.src = imageSrc;
   captionElement.innerHTML = caption;
   modal.style.display = 'block';
 
-  // Close modal when overlay (dark background) is clicked
+  // Close modal on overlay click
   modalOverlay.addEventListener('click', closeFullScreen);
+
+  // Swipe down to close modal
+  let startY;
+  modal.addEventListener('touchstart', (e) => {
+    startY = e.touches[0].clientY;
+  });
+
+  modal.addEventListener('touchmove', (e) => {
+    if (e.touches[0].clientY - startY > 50) {
+      closeFullScreen();
+    }
+  });
 }
 
 // Close Fullscreen Modal
-function closeFullScreen(event) {
+function closeFullScreen() {
   const modal = document.getElementById('fullscreen-modal');
   modal.style.display = 'none';
 }
 
 // Open Birthday Year
 function openYear(year) {
-  // Add logic to navigate to the selected year
-  // For now, we'll just alert the selected year
   alert(`Opening birthday cards for ${year}`);
 }
+
 document.addEventListener('DOMContentLoaded', function () {
   const scrollableBox = document.getElementById('scrollable-box');
 
-  // Sample data for images and captions
   const imageData = [
     { src: 'images/Image1.jpg', caption: 'Chris happy birthday! I got you a present!' },
     { src: 'images/Image2.jpg', caption: 'La rata watching 2' },
@@ -46,7 +56,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add more objects for additional images
   ];
 
-  // Dynamically create sub-boxes
   imageData.forEach((data, index) => {
     const subBox = document.createElement('div');
     subBox.classList.add('sub-box');
@@ -60,4 +69,3 @@ document.addEventListener('DOMContentLoaded', function () {
     scrollableBox.appendChild(subBox);
   });
 });
-
